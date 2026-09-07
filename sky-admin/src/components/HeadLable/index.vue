d1fc87e45ca3d2da6fe0adbb6a3a1da0fe4ed6e9
anguish <2713786149@qq.com>
2026-09-08 00:48:01 +0800

    fix(layout): 添加员工页头改 flex 布局,箭头与文字对齐并补返回分隔竖线

--- sky-admin/src/components/HeadLable/index.vue
+++ sky-admin/src/components/HeadLable/index.vue
@@ -30,3 +30,32 @@ const goBack = () => {
   router.go(-1)
 }
 </script>
+
+<style lang="scss" scoped>
+/* 与 Vue2 原版一致的页头:64px 行高、16px 加粗标题、18px 返回图标、
+   返回与标题间有 1px #D8DDE3 竖线分隔 */
+.HeadLable {
+  display: flex;
+  align-items: center;
+  height: 64px;
+  font-size: 16px;
+  font-weight: 700;
+  color: #333;
+
+  .goBack {
+    display: flex;
+    align-items: center;
+    font-weight: 400;
+    padding-right: 14px;
+    margin-right: 14px;
+    border-right: 1px solid #d8dde3;
+    cursor: pointer;
+
+    img {
+      width: 18px;
+      height: 18px;
+      margin-right: 5px;
+    }
+  }
+}
+</style>
